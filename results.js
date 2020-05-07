347553508f3d3521bfd83b992754649f7589b604
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-05-06 21:29:02 -0400

    This is my updated commit with added comments

--- results.js
+++ results.js
@@ -1,31 +1,30 @@
-// var displayScore = document.querySelector("#score-display");
+// var displayScore = document.querySelector("#score-display")
 var displayScore = document.querySelector("#score-list");
 
 // assigning  my array of objects  to a variable to manipulate on this page
-var lastUser = JSON.parse(localStorage.getItem("userArr")); 
+var lastUser = JSON.parse(localStorage.getItem("userArr"));
 // setting a renderScores function to then turn my inputs into list items
- function renderScores() {
-        // Clear todoList element and update todoCountSpan
-        displayScore.textContent = lastUser.length;
-      
-        // Render a new li for each todo
-        for (var i = 0; i < lastUser.length; i++) {
-          var display = lastUser[i];
-      
-          var li = document.createElement("li");
-          li.textContent = display;
-          li.setAttribute("data-index", i);
-          displayScore.appendChild(li);
-        }
-      }
+function renderScores() {
+  // Clear todoList element and update todoCountSpan
+  displayScore.textContent = lastUser.length;
+
+  // Render a new li for each todo
+  for (var i = 0; i < lastUser.length; i++) {
+    var display = lastUser[i];
+
+    var li = document.createElement("li");
+    li.textContent = display;
+    li.setAttribute("data-index", i);
+    displayScore.appendChild(li);
+  }
+}
 console.log(lastUser);
 renderScores();
-// linking my submitbutton 
-var initBtn = function(){ 
-    
-}
+// linking my submitbutton
+var initBtn = function () {};
 
-document.addEventListener('DOMContentLoaded', function(){
-initBtn();});
+document.addEventListener("DOMContentLoaded", function () {
+  initBtn();
+});
 
 initBtn();
